d797da4ae64059d55712f3396aa60b2bc1cee03a
Rick van Sloten <rickvansloten@gmail.com>
2021-06-30 17:23:03 +0200

    Appended setup configuration and application metadata.

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from setuptools import setup
+from setuptools import find_packages
 
 setup(
     name='virtual_item_price_database',
-    packages=['virtual_item_price_database'],
+    version='1.0.0',
+    description='This package is a Flask REST API for virtual item data, and market prices.',
+    author='Rick van Sloten',
+    author_email='rick@tabuu.nl',
+    url='https://github.com/Tabuu/virtual_item_price_database',
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'flask',
-        'flask_sqlalchemy',
-        'selenium',
-        'bs4',
-        'requests',
-        'sqlalchemy',
+        'bs4',                  # Parsing HTML.
+        'requests',             # Making JSON requests.
+        'flask',                # General REST API framework.
+        'flask_sqlalchemy',     # Flask version of SQLAlchemy.
+        'selenium',             # Webpage renderer to scrape pages that need JavaScript rendering.
+        'sqlalchemy',           # Managing the database structure, and CRUD functionality.
     ]
 )
